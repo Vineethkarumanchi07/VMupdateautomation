6c54e184d5ca809dccca0426ac5c0b35170a729d
vineeth karumanchi <vineethkarumanchi118@outlook.com>
2023-08-14 01:41:04 +0100

    log analytics solution is added

--- terraform/log_analytics_workspace.tf
+++ terraform/log_analytics_workspace.tf
@@ -13,25 +13,25 @@ resource "azurerm_log_analytics_workspace" "terra-autoAcc" {
   retention_in_days   = 30
 }
 
-# # Create a log analytics workspace (enable the log analytics space to be holding the updates)
-# resource "azurerm_log_analytics_solution" "terra-autoAcc" {
-#   solution_name         = "Updates"
-#   location              = azurerm_log_analytics_workspace.terra-autoAcc.location
-#   resource_group_name   = azurerm_resource_group.terraform-acc.name
-#   workspace_resource_id = azurerm_log_analytics_workspace.terra-autoAcc.id
-#   workspace_name        = azurerm_log_analytics_workspace.terra-autoAcc.name
+# Create a log analytics workspace (enable the log analytics space to be holding the updates)
+resource "azurerm_log_analytics_solution" "terra-autoAcc" {
+  solution_name         = "Updates"
+  location              = azurerm_log_analytics_workspace.terra-autoAcc.location
+  resource_group_name   = azurerm_resource_group.terraform-acc.name
+  workspace_resource_id = azurerm_log_analytics_workspace.terra-autoAcc.id
+  workspace_name        = azurerm_log_analytics_workspace.terra-autoAcc.name
 
-#   plan {
-#     publisher = "Microsoft"
-#     product   = "OMSGallery/Updates"
-#   }
-# }
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/Updates"
+  }
+}
 
-# # Create a azurerm_log_analytics_linked_service(ensure link log analytics to automation account)
-# resource "azurerm_log_analytics_linked_service" "terra-autoAcc" {
-#   resource_group_name = azurerm_resource_group.terraform-acc.name
-#   workspace_id        = azurerm_log_analytics_workspace.terra-autoAcc.id
-#   read_access_id      = azurerm_automation_account.terra-autoAcc.id
+# Create a azurerm_log_analytics_linked_service(ensure link log analytics to automation account)
+resource "azurerm_log_analytics_linked_service" "terra-autoAcc" {
+  resource_group_name = azurerm_resource_group.terraform-acc.name
+  workspace_id        = azurerm_log_analytics_workspace.terra-autoAcc.id
+  read_access_id      = azurerm_automation_account.terra-autoAcc.id
   
-#   depends_on = [azurerm_log_analytics_solution.terra-autoAcc]
-# }
+  depends_on = [azurerm_log_analytics_solution.terra-autoAcc]
+}
